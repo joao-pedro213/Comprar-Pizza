b8afb1d44f109c302c4190369246fee7b8f87f72
joao-pedro213 <joao.capobiango213@gmail.com>
2020-05-09 21:23:51 -0300

    Adicionando os dados na estrutura.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-pizzaJson.map((item, index) => {
-  let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
-  //preencher as informações em pizzaitem
+const query = (element) => document.querySelector(element);
+const queryAll = (element) => document.querySelectorAll(element);
 
-  document.querySelector('.pizza-area').append( pizzaItem );
+pizzaJson.map((item, index) => {
+  let pizzaItem = query('.models .pizza-item').cloneNode(true);
+
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  query('.pizza-area').append( pizzaItem );
 });
+
+
