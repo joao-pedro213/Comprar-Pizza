9dbcd63969900c28d38c27088c3583b2dc8b56cf
joao-pedro213 <joao.capobiango213@gmail.com>
2020-05-09 22:34:56 -0300

    Adicionando as informações da pizza nos modal de acordo com o ID de cada uma.

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const queryAll = (element) => document.querySelectorAll(element);
 pizzaJson.map((item, index) => {
   let pizzaItem = query('.models .pizza-item').cloneNode(true);
 
+  pizzaItem.setAttribute('data-key', index);
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
   pizzaItem.querySelector('a').addEventListener('click', (e)=> {
     e.preventDefault();
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+    query('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    query('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+    query('.pizzaBig img').src = pizzaJson[key].img;
 
     query('.pizzaWindowArea').style.opacity = 0;
     query('.pizzaWindowArea').style.display = 'flex';
